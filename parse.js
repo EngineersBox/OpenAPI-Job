1f3146a513ea5ffafa8bfcf8d2bfedc9f1ea7694
Jack Kilrain <jack.kilrain@instaclustr.com>
2020-04-15 10:44:42 +1000

    Added support for verbose logging and update npm build scripts

--- parse.js
+++ parse.js
@@ -5,20 +5,26 @@ const OpenAPISnippet = require('openapi-snippet');
 const yaml = require('js-yaml');
 const clc = require('cli-color');
 
-const error = function (msg) {
-	console.log("[" + clc.bgRed.white.bold("Error!") + "]", msg);
+let isVerbose = false;
+
+function generateTreeLines(nest) {
+	return nest > 0 ? "|" + " |".repeat(Math.max(0, nest - 1)) + "-->" : "";
+}
+
+const error = function (msg, nest=0) {
+	console.log(generateTreeLines(nest) + "[" + clc.bgRed.white.bold("Error!") + "]", msg);
 };
-const warning = function (msg) {
-	console.log("[" + clc.bgYellow.white.bold("Warning") + "]", msg);
+const warning = function (msg, nest=0) {
+	console.log(generateTreeLines(nest) + "[" + clc.bgYellow.white.bold("Warning") + "]", msg);
 };
-const progress = function (msg) {
-	console.log("[" + clc.bgBlue.white.bold("Info") + "]", msg);
+const progress = function (msg, nest=0) {
+	console.log(generateTreeLines(nest) + "[" + clc.bgBlue.white.bold("Info") + "]", msg);
 };
-const complete = function (msg) {
-	console.log("[" + clc.bgGreen.white.bold("Completed") + "]", msg);
+const complete = function (msg, nest=0) {
+	console.log(generateTreeLines(nest) + "[" + clc.bgGreen.white.bold("Completed") + "]", msg);
 };
-const usage = function (msg) {
-	console.log("[" + clc.bgMagenta.white.bold("Usage") + "]", msg);
+const usage = function (msg, nest=0) {
+	console.log(generateTreeLines(nest) + "[" + clc.bgMagenta.white.bold("Usage") + "]", msg);
 };
 
 const validTargets = [
@@ -67,14 +73,27 @@ function enrichSchema(schema) {
 	for (var path in schema.paths) {
 		for (var method in schema.paths[path]) {
 			var generatedCode = OpenAPISnippet.getEndpointSnippets(schema, path, method, targets);
-			if (!schema.paths[path][method]["x-code-samples"])
+			if (isVerbose)
+				progress("Checking existance of 'x-code-samples' field in " + path + " path...");
+			if (!schema.paths[path][method]["x-code-samples"]) {
+				if (isVerbose)
+					complete("Added 'x-code-samples' field.", 1);
 				schema.paths[path][method]["x-code-samples"] = [];
+			} else {
+				if (isVerbose)
+					warning("'x-code-samples' field already exists, skipping.", 1);
+			}
 			for (var snippetIdx in generatedCode.snippets) {
 				var snippet = generatedCode.snippets[snippetIdx];
-				if (!schema.paths[path][method]["x-code-samples"][snippetIdx])
+				if (isVerbose)
+					progress("Checking existance of '" + snippet.title + "' field...", 1);
+				if (!schema.paths[path][method]["x-code-samples"][snippetIdx]) {
+					if (isVerbose)
+						complete("Added '" + snippet.title + "' field.", 2);
 					schema.paths[path][method]["x-code-samples"][snippetIdx] = { "lang": snippet.title, "source": snippet.content };
-				else
-					warning("Field '" + snippet.title + "' already exists in '" + path + "' path, skipping.");
+				} else {
+					warning("Field '" + snippet.title + "' already exists in '" + path + "' path, skipping.", 2);
+				}
 			}
 
 		}
@@ -85,29 +104,38 @@ function enrichSchema(schema) {
 // Check the OpenAPI spec is given
 if (process.argv.length < 3) {
 	error("Please pass the OpenAPI JSON schema as argument.");
-	usage("npm run add-examples <swagger/openapi specification> <output destination> {[targets]}\n");
+	usage("npm run add-examples <swagger/openapi specification> <output destination> {[targets]} {options}\n");
 	process.exit(1);
 }
 
 // Check an output file/destination is specified
 if (process.argv.length < 4) {
 	error("Please specify an output file.");
-	usage("npm run add-examples <swagger/openapi specification> <output destination> {[targets]}\n");
+	usage("npm run add-examples <swagger/openapi specification> <output destination> {[targets]} {options}\n");
 	process.exit(1);
+}
+
+if (process.argv[process.argv.length - 1] === "-v" || process.argv[process.argv.length - 1] === "-verbose") {
+	progress("Switching to verbose logging");
+	isVerbose = true;
 }
 
 // Configure custom targets if specified
 if (process.argv.length > 4) {
 	progress("Validating targets...");
-	let targetsToCheck = process.argv.slice(4);
-	for (const target of targetsToCheck) {
-		if (!targets.includes(target)) {
-			error("Target \'" + target + "\' is not valid");
-			process.exit(1);
+	if (process.argv[4] === "default")
+		complete("Default specified, skipping.", 1);
+	else {
+		let targetsToCheck = process.argv.slice(4);
+		for (const target of targetsToCheck) {
+			if (!targets.includes(target)) {
+				error("Target \'" + target + "\' is not valid");
+				process.exit(1);
+			}
 		}
+		targets = targetsToCheck;
+		complete("All specified targets are valid", 1);
 	}
-	targets = targetsToCheck;
-	complete("All specified targets are valid");
 } else {
 	warning("No targets specified, defaulting to internally specified:" + targets.map(v => ' '+ v));
 }
@@ -115,7 +143,7 @@ if (process.argv.length > 4) {
 // Check output file is in JSON format
 if (process.argv[3].indexOf('json') == -1) {
 	error("Only JSON format is supported for output.");
-	usage("npm run add-examples <swagger/openapi specification> <output destination> {[targets]}\n");
+	usage("npm run add-examples <swagger/openapi specification> <output destination> {[targets]} {options}\n");
 	process.exit(1);
 }
 
@@ -125,11 +153,11 @@ const specPath = "./specification.json";
 try {
 	progress("Checking if specification.json exists");
 	if (fs.existsSync(specPath)) {
-		progress("specification.json exists, removing...");
+		progress("specification.json exists, removing...", 1);
 		fs.unlinkSync(specPath);
-		complete("specification.json removed");
+		complete("specification.json removed", 1);
 	} else {
-		complete("specification.json does not exist, continuing");
+		complete("specification.json does not exist, continuing", 1);
 	}
 } catch(err) {
 	error(err);
@@ -141,12 +169,12 @@ if (process.argv[2].indexOf('yml') !== -1 || process.argv[2].indexOf('yaml') !==
 	try {
 		progress("Converting YAML file...");
 		let schema = yaml.safeLoad(fs.readFileSync(process.argv[2], 'utf8'));
-		complete("Converted YAML file");
-		progress("Adding samples for " + String(targets).replace(/,/g, ", ") + "...");
+		complete("Converted YAML file", 1);
+		progress("Adding samples for " + String(targets).replace(/,/g, ", ") + "...", 1);
 		schema = enrichSchema(schema);
 		fs.writeFile(process.argv[3], JSON.stringify(schema), function (err) {
 			if (err) throw err;
-			complete("Added samples and exported to: " + process.argv[3]);
+			complete("Added samples and exported to: " + process.argv[3], 2);
 		});
 	} catch (e) {
 		error(e);
@@ -159,11 +187,11 @@ if (process.argv[2].indexOf('yml') !== -1 || process.argv[2].indexOf('yaml') !==
 			if (err) throw err;
 			let schema = JSON.parse(data);
 			schema = enrichSchema(schema);
-			complete("Parsed JSON file");
-			progress("Adding samples for " + String(targets).replace(/,/g, ", ") + "...");
+			complete("Parsed JSON file", 1);
+			progress("Adding samples for " + String(targets).replace(/,/g, ", ") + "...", 1);
 			fs.writeFile(process.argv[3], JSON.stringify(schema), function (err) {
 				if (err) throw err;
-				complete("Added samples and exported to: " + process.argv[3]);
+				complete("Added samples and exported to: " + process.argv[3], 2);
 			});
 		});
 	} catch (e) {
